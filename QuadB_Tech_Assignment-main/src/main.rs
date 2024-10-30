a0d61057321dfe3a9162007efb739c7c74d57f41
Bhumika Borigi <155643399+bhumiborigi@users.noreply.github.com>
2024-10-30 11:12:13 +0530

    Update main.rs

--- QuadB_Tech_Assignment-main/src/main.rs
+++ QuadB_Tech_Assignment-main/src/main.rs
@@ -3,15 +3,15 @@ mod token_wallet;
 use token_wallet::Wallet;
 
 fn main() {
-    let alice_wallet = Wallet::new();
+    let bhumi_wallet = Wallet::new();
     let bob_wallet = Wallet::new();
 
-    alice_wallet.receive_tokens(100);  // Add initial balance to Alice's wallet
-    match alice_wallet.send_tokens(50, &bob_wallet) {
+    bhumi_wallet.receive_tokens(100);  // Add initial balance to Alice's wallet
+    match bhumi_wallet.send_tokens(50, &bob_wallet) {
         Ok(_) => println!("Tokens sent successfully!"),
         Err(e) => println!("Failed to send tokens: {}", e),
     }
 
-    println!("Alice's balance: {}", alice_wallet.get_balance());
+    println!("Bhumi's balance: {}", bhumi_wallet.get_balance());
     println!("Bob's balance: {}", bob_wallet.get_balance());
 }
